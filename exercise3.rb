df90ded05076c36f36c7aaf09ee456a3af57d9c0
Phil Usprech <phil@Macintosh.local>
2016-05-31 15:50:23 -0400

    Added lines and responses for last name and age

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,12 @@
-puts "What is your name?"
-name = "blah" #     gets.chomp
-puts "Hi #{name}!"
+puts "What is your first name?"
+first_name = gets.chomp
+	puts "Hi #{first_name}!"
+
+puts "How about your last name?"
+last_name = gets.chomp
+	puts "Great! Welcome #{first_name} #{last_name}!"
+
+puts "How old are you?"
+age = gets.chomp
+	puts "#{age} is a great age!"
+
