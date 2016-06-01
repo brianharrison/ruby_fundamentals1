90c013cf21e9dfa14414ceeb3508abf9bec56206
Brian Harrison <brianharrison@rogers.com>
2016-05-31 22:17:37 -0400

    Switched some variables around and modified range

--- exercise4.rb
+++ exercise4.rb
@@ -1,12 +1,12 @@
 100.times do |number|
 plus1 = number+1
 
-if(plus1 % 3 ==0)
+if(plus1 % 3 ==0 && plus1 % 5 ==0)
+	puts "BitMaker"
+elsif (plus1 % 3 ==0)
 	puts "Bit"
 elsif (plus1 % 5 ==0)
 	puts "Maker"
-elsif (plus1 % 3 ==0 and plus1 % 5 ==0)
-	puts "BitMaker"
 else
 	puts plus1
 end
